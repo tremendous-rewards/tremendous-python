093bc336db699f996f8d2a17598d3c7c26f002a2
Rahul Mehta <mehta.rahul95@gmail.com>
2021-04-06 11:32:45 -0400

    Upgrade deps

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
     license='MIT',
     packages=find_packages(exclude='tests'),
     package_data={'README': ['README.md']},
-    install_requires=['requests==2.20.0'],
+    install_requires=['requests>=2.20.0'],
     zip_safe=False,
     include_package_data=True,
     classifiers=[
